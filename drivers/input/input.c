e71729e700e4605da6404a3ebe700e35cfda8b4e
Jesse Chan <jc@lineageos.org>
2018-11-06 19:53:06 +0000

    star: disable input boosting

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -846,7 +846,9 @@ void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
+#if !defined(CONFIG_INPUT_BOOSTER) // Input Booster +
 	int idx;
+#endif  // Input Booster -
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
